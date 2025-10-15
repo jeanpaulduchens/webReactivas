2d94a43d46d94c2be3974f58b3b0e1d91b4d1178
jeanpaulduchens <jean.duchens@ug.uchile.cl>
2025-10-14 22:25:35 -0300

    add: Hero component

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1 className="hero-title">Bienvenido a BarberBook: Tu Estilo, Nuestra Pasión</h1>
+        <p className="hero-sub">
+          Encuentra tu servicio ideal y reserva tu cita con los mejores barberos de la ciudad.
+          Calidad y tradición a tu alcance.
+        </p>
+      </div>
+    </section>
+  );
+}
