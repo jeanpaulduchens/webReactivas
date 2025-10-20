ea9e011796277acb6a7720ba3c892546a3e8fbb2
Dagt07 <davidgarciat07@gmail.com>
2025-10-20 15:44:05 -0300

    Feat: Se agregan usuarios por Rol
    Se deja de momento las opciones de rol en 'cliente' o 'barbero' por simpleza de esta iteración

---
+++ backend/src/models/users.ts
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+
+export interface UserData {
+  id: string;
+  username: string;
+  name: string;
+  email: string;
+  passwordHash: string;
+  role: 'cliente' | 'barbero';
+  phone?: string;
+  reservations?: mongoose.Types.ObjectId[]; // clientes
+  createdAt: Date;
+}
+
+const userSchema = new mongoose.Schema<UserData>({
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    minlength: 3
+  },
+  name: { 
+    type: String, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  },
+  passwordHash: { 
+    type: String, 
+    required: true 
+  },
+  role: {
+    type: String,
+    enum: ['cliente', 'barbero'],
+    default: 'cliente',
+    required: true
+  },
+  phone: {
+    type: String,
+    required: false
+  },
+  reservations: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Reservation",
+    },
+  ],
+  timestamps: true // Agrega createdAt y updatedAt automáticamente
+});
+
+userSchema.set("toJSON", {
+  transform: (
+    document,
+    returnedObject: {
+      id?: string;
+      _id?: mongoose.Types.ObjectId;
+      __v?: number;
+      passwordHash?: string;
+    }
+  ) => {
+    returnedObject.id = returnedObject._id?.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash; // Para no exponer el hash
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
